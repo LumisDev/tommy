f8f3afed9cb3291761fd5fcb03c2380d0cd802c2
LumisDev <153009693+LumisDev@users.noreply.github.com>
2024-02-11 20:09:37 +0100

    Update batch automator

--- build-win.bat
+++ build-win.bat
@@ -6,16 +6,20 @@ cls
 echo Choose a generator:
 echo 1. MinGW
 echo 2. VS2019
-echo 3. Exit
+echo 3. Anything else
+echo 4. Exit
 
 set /p choice="Enter your choice: "
 
 if "%choice%"=="1" (
-    call :build "mingw" "MinGW Makefiles"
-) elseif "%choice%"=="2" (
-    call :build "vs" "Visual Studio 2019"
-) elseif "%choice%"=="3" (
-    exit
+    call :build "mingw"
+) else if "%choice%"=="2" (
+    call :build "vs"
+) else if "%choice%"=="3" (
+    set /p b-type="Can you describe the type please?:"
+    call :build-altern %b-type%
+) else if "%choice%"=="4" (
+    exit 
 ) else (
     echo Invalid choice. Please try again.
     pause
@@ -26,5 +30,15 @@ exit /b
 
 :build
 mkdir "build-%~1" 2>nul
-cd "build-%~1" || exit /b
-cmake .. -G %~2
+cd "build-%~1"
+if %~1=="mingw"(
+    cmake .. -G "MinGW Makefiles"
+) else if %~1=="vs" (
+    cmake .. -G "Visual Studio 2019"
+)
+
+
+:build-altern
+mkdir "build" 2> nul
+cd build
+cmake .. -G %~1
